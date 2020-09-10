3f503609a007ef5a1595d783468321706d5ffc83
COrtaDev <COrtaDev@gmail.com>
2020-09-10 11:47:07 -0400

    Commit Before Debug/Refactor

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,71 +1,65 @@
 import bcrypt
 from flask import Blueprint, jsonify, request
-from flask_cors import cross_origin
 from app.models import db, User
 from app.auth import create_jwt, validate_jwt
 
 user_routes = Blueprint('users', __name__)
 
 
-@user_routes.route('/')
-def index():
-    response = User.query.all()
-    return {"users": [user.to_dict() for user in response]}
+# @user_routes.route('/')
+# def index():
+#     response = User.query.all()
+#     return {"users": [user.to_dict() for user in response]}
 
 
-@user_routes.route('', methods=['post'])
-@cross_origin(resources={r"/api/*": {"origins": "localhost"}})
-def sign_up():
+@user_routes.route('', methods=['POST'])
+def sign_up(request):
     data = request.json
-    print(data)
     # Create a hashed password
     password = data['password'].encode()
     hashed_password = bcrypt.hashpw(
         password, bcrypt.gensalt(14)).decode('utf-8')
     # Generate and add new user to db
-    try:
-        new_user = User(username=data['userName'],
-                        email=data['email'],
-                        hashed_password=hashed_password)
-        db.session.add(new_user)
-        db.session.flush()
-    except:
-        raise RuntimeError("Bad Submission")
+    new_user = User(username=data['userName'],
+                    email=data['email'],
+                    hashed_password=hashed_password)
+    db.session()
+    db.session.add(new_user)
+    db.session.commit()
     # get user and create jwt to return
     user = User.query.filter(User.email == data['email']).first().to_dict()
     jwt = create_jwt(user)
     # only commit the new user after everything passes with no errors
-    db.session.commit()
     return jsonify({"user": user, "token": str(jwt)})
 
 
-@ user_routes.route('/login', methods=['post'])
-def login():
-    data = request.json
-    user = User.query.filter(User.email == data['email']).first()
-    print(user)
-    hashed_password = user.hashed_password
-    if bcrypt.checkpw(data['password'].encode(), hashed_password.encode()):
+# @ user_routes.route('/login', methods=['post'])
+# def login():
+#     data = request.json
+#     user = User.query.filter(User.email == data['email']).first()
+#     print(user)
+#     hashed_password = user.hashed_password
+#     if bcrypt.checkpw(data['password'].encode(), hashed_password.encode()):
 
-        user_data = user.to_dict()
-        jwt = create_jwt(user_data)
-        return jsonify({"user": user_data, "token": str(jwt)})
-    else:
+#         user_data = user.to_dict()
+#         jwt = create_jwt(user_data)
+#         return jsonify({"user": user_data, "token": str(jwt)})
+#     else:
 
-        return jsonify('Bad Login')
+#         return jsonify('Bad Login')
 
 
-@ user_routes.route('/restore')
-# @cross_origin(allow_headers=['Content-Type'])
-def restore():
-    auth_header = request.headers['Authorization']
-    print(auth_header)
-    print(auth_header[7:])
-    validated = validate_jwt(request)
-    if (validated):
-        return validated
-    else:
-        return jsonify(None)
+# @ user_routes.route('/restore')
+# # @cross_origin(allow_headers=['Content-Type'])
+# def restore():
+#     # auth_header = request.headers['Authorization']
+#     # print(auth_header)
+#     # print(auth_header[7:])
+#     validated = validate_jwt(request)
+#     if (validated):
+#         return validated
+#     else:
+#         return jsonify(None)
 
 
 # @user_routes.route('/query/<user_id>/<query>')
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
 export const Home = () => {
+    const { token } = useSelector(state => state.auth);
+
+    if (token) {
+        return <Redirect to="/users"></Redirect>;
+    };
 
     return (
         <>
