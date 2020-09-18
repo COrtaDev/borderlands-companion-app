8bb174b5a78959fac73b45f22c4818b369cc7efb
COrtaDev <COrtaDev@gmail.com>
2020-09-18 19:32:27 -0400

    Pausing because tired.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { hasAccessToken } from './actions/auth';
 import Landing from './components/Landing';
-import Portal from './components/Portal';
+// import Portal from './components/Portal';
+import ItemDropdown from './components/sub-components/ItemDropdown'
 import PortalModal from './components/modals/PortalModal';
 import LogoutButton from './components/sub-components/LogoutButton';
 import LootDropMockup from './components/sub-components/LootDropMockup';
@@ -19,7 +20,7 @@ const App = () => {
             <nav>
                 <ul>
                     <li><NavLink to="/" activeclass="active">Landing</NavLink></li>
-                    <li><NavLink to="/portal" activeclass="active">Login/Signup Portal</NavLink></li>
+                    {/* <li><NavLink to="/customtoggle" activeclass="active">custom toggle</NavLink></li> */}
                     <li><NavLink to="/home" activeclass="active">Home</NavLink></li>
                     <li><NavLink to="/lootdrop" activeclass="active">Loot Drop</NavLink></li>
                     <li><NavLink to="/portalmodal" activeclass="active">Portal Modal</NavLink></li>
@@ -28,7 +29,7 @@ const App = () => {
             </nav>
             <Switch>
                 <Route exact path="/" component={Landing} />
-                <Route path="/portal" component={Portal} />
+                {/* <Route path="/customtoggle" component={ItemDropdown} /> */}
                 <Route path="/home"><Home userId={userId} userName={userName} /></Route>
                 <Route path="/lootdrop"><LootDropMockup userId={userId} userName={userName} /></Route>
                 <Route path="/portalmodal" component={PortalModal}></Route>
