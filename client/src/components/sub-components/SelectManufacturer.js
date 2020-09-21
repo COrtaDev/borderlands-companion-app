c889542e01e43e209f5603456586d1f4e88d2d3c
COrtaDev <COrtaDev@gmail.com>
2020-09-21 00:35:15 -0400

    Can filter by multiple grenade Manufacturers

--- client/src/components/sub-components/SelectManufacturer.js
+++ client/src/components/sub-components/SelectManufacturer.js
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { SET_ITEM_MANUFACTURER } from '../../actions/lootDrops';
 import { Button, DropdownButton, Dropdown, ButtonGroup, } from 'react-bootstrap';
 import { manufacturers } from '../modal-assets/manufacturers';
+import { possibleGrenadeManufacturers } from '../modal-assets/grenades';
 // import { SET_ITEM_TYPE } from '../../actions/lootDrops';
 const SelectManufacturer = (props) => {
     const dispatch = useDispatch();
-    let { itemType } = useSelector(state => state.lootDrops);
+    let { itemType, itemName } = useSelector(state => state.lootDrops);
     const [itemManufacturer, setItemManufacturer] = useState(null)
     const [manufacturerButtonVariant, setManufacturerButtonVariant] = useState("danger")
     const [confirmButtonVariant, setConfirmButtonVariant] = useState("secondary")
@@ -24,9 +25,19 @@ const SelectManufacturer = (props) => {
         props.onHide();
     }
     let DropdownItems = [];
-    if (itemType) {
+    // console.log(itemType)
+    // console.log(itemName)
+    if (itemType === "Grenade Mod" && itemName !== '') {
+        // console.log("potentialManufacturers")
+        const grenade = possibleGrenadeManufacturers.filter((grenade) => grenade.name === itemName);
+        const [manufacturers] = grenade;
+        // console.log(grenade)
+        // console.log(manufacturers)
+        // console.log(manufacturers.possibleManufacturers)
+        DropdownItems = manufacturers.possibleManufacturers.map((grenade) => <Dropdown.Item eventKey={grenade}>{grenade}</Dropdown.Item>)
+    } else if (itemType && itemType !== "Grenade Mod") {
         const potentialItems = manufacturers.filter((item) => item.type === itemType)
-        console.log(potentialItems)
+        // console.log(potentialItems)
         DropdownItems = potentialItems.map((item) => <Dropdown.Item key={item.id} eventKey={item.manufacturer}>{item.manufacturer}</Dropdown.Item>)
     } else {
         DropdownItems = manufacturers.map((item) => <Dropdown.Item key={item.id} eventKey={item.manufacturer} >{item.namanufacturerme}</Dropdown.Item>)
