c726c6e259fefa89453437e7d80d77b8c3f8b31b
COrtaDev <COrtaDev@gmail.com>
2020-09-29 17:22:50 -0400

    Resolved a multiple warnings

--- client/src/components/sub-components/SideNav.js
+++ client/src/components/sub-components/SideNav.js
@@ -7,46 +7,46 @@ const SideNav = () => {
 
     return (
         <>
-            <aside id='sideNav' class='menu'>
-                <ul class='list'>
-                    <div class='menu menu-item'>
+            <aside id='sideNav' className='menu'>
+                <ul className='list'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faCaravan} size='2x' opacity='1' />Home
                             </a>
                         </li>
                     </div>
-                    <div class='menu menu-item'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faCrosshairs} size='2x' opacity='1' />Explore
                             </a>
                         </li>
                     </div>
-                    <div class='menu menu-item'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faBomb} size='2x' opacity='1' />Likes
                             </a>
                         </li>
                     </div>
-                    <div class='menu menu-item'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faUserSecret} size='2x' opacity='1' />Follows
                             </a>
                         </li>
                     </div>
-                    <div class='menu menu-item'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faCommentDollar} size='2x' opacity='1' />Comments
                             </a>
                         </li>
                     </div>
-                    <div class='menu menu-item'>
+                    <div className='menu menu-item'>
                         <li id='list-item'>
-                            <a class='list list-item'>
+                            <a className='list list-item'>
                                 <FontAwesomeIcon id='icons' icon={faPiggyBank} size='2x' opacity='1' />Your Loot
                             </a>
                         </li>
