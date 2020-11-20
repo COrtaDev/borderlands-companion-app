b5698ebb50a3c7850aabd1508d4ff98bdbb329aa
COrtaDev <COrtaDev@gmail.com>
2020-11-20 16:54:04 -0500

    Item Dropdown component given additional comments

--- client/src/components/sub-components/ItemDropdown.js
+++ client/src/components/sub-components/ItemDropdown.js
@@ -2,9 +2,7 @@ import React, { forwardRef, useState, FormControl, Children } from 'react';
 // import Dropdown from 'react-bootstrap/Dropdown';
 
 const ItemDropdowm = ()=>{
-
+//There will be nothing to add here for the time being.
 }
 
 export default ItemDropdowm;
-
-
